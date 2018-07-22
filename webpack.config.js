d5f5bda14908d1d7b321723799c624bdd9d3fd55
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-07-22 22:13:34 +0800

    Set up the HTML webpack plugin
    Now, dist contains all the files to be served over the web.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm.js',
     },
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 };
