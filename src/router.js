9296640dee2db65beeb005fef561d630531844da
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-07-23 23:08:37 +0800

    Code split all the routes

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import Account from './views/Account.vue';
-import CreateCard from './views/CreateCard.js';
 
 Vue.use(VueRouter);
 
 const Decks = () => import('./views/Decks.js');
 const Practice = () => import('./views/Practice.js');
+const CreateCard = () => import('./views/CreateCard.js');
+const Account = () => import('./views/Account.vue');
 
 export default new VueRouter({
   routes: [
