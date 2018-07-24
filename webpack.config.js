9c6df3b0103bb732b6ab4b6adfc89491b4aef8f9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-07-24 21:54:27 +0800

    Improve code splitting and caching

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
+  output: {
+    filename: '[name].[chunkhash].js',
+  },
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js',
@@ -12,7 +17,10 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      inlineSource: 'runtime~.+\\.js',
     }),
+    new InlineSourcePlugin(),
+    new webpack.HashedModuleIdsPlugin(),
     new VueLoaderPlugin(),
   ],
   module: {
@@ -30,5 +38,11 @@ module.exports = {
         ],
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
