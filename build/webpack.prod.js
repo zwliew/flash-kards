1c2c0f572e61b5590aedac2e8bc567707f920215
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-07-27 18:33:16 +0800

    Define production mode for webpack
    Gets rid of the annoying warning.

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  mode: 'production',
   output: {
     filename: '[name].[chunkhash].js',
   },
