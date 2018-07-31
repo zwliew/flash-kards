0c9113dc4f7e1bc0268a91862f77ae1f1de90e43
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-07-31 14:43:27 +0800

    Enable Babel-loader cache

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader?cacheDirectory',
       },
     ],
   },
